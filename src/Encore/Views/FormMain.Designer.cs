e81f0275033f5a73654f54238bc949dbc27d3c0a
John Ellis <johnellisperth@gmail.com>
2022-02-03 23:06:14 +0800

    gui fixes. Now using tablelayout panels and panels for better placement

--- src/Encore/Views/FormMain.Designer.cs
+++ src/Encore/Views/FormMain.Designer.cs
@@ -33,7 +33,6 @@ namespace Encore
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.buttonBackup = new System.Windows.Forms.Button();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.comboBoxDrive = new System.Windows.Forms.ComboBox();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.comboBoxBackup = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -41,13 +40,25 @@ namespace Encore
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBoxDrive = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonCompare
             // 
-            this.buttonCompare.Location = new System.Drawing.Point(81, 253);
+            this.buttonCompare.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonCompare.Location = new System.Drawing.Point(53, 18);
             this.buttonCompare.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.buttonCompare.Name = "buttonCompare";
             this.buttonCompare.Size = new System.Drawing.Size(401, 85);
@@ -64,17 +75,18 @@ namespace Encore
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(867, 112);
+            this.dataGridView1.Location = new System.Drawing.Point(7, 823);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 102;
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(1471, 490);
+            this.dataGridView1.Size = new System.Drawing.Size(1777, 699);
             this.dataGridView1.TabIndex = 3;
             // 
             // buttonBackup
             // 
-            this.buttonBackup.Location = new System.Drawing.Point(81, 382);
+            this.buttonBackup.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonBackup.Location = new System.Drawing.Point(53, 146);
             this.buttonBackup.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.buttonBackup.Name = "buttonBackup";
             this.buttonBackup.Size = new System.Drawing.Size(401, 85);
@@ -91,36 +103,29 @@ namespace Encore
             this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(867, 728);
+            this.dataGridView2.Location = new System.Drawing.Point(7, 58);
             this.dataGridView2.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 102;
             this.dataGridView2.RowTemplate.Height = 25;
-            this.dataGridView2.Size = new System.Drawing.Size(1471, 491);
+            this.dataGridView2.Size = new System.Drawing.Size(1777, 699);
             this.dataGridView2.TabIndex = 4;
-            // 
-            // comboBoxDrive
-            // 
-            this.comboBoxDrive.FormattingEnabled = true;
-            this.comboBoxDrive.Location = new System.Drawing.Point(238, 52);
-            this.comboBoxDrive.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
-            this.comboBoxDrive.Name = "comboBoxDrive";
-            this.comboBoxDrive.Size = new System.Drawing.Size(339, 49);
-            this.comboBoxDrive.TabIndex = 6;
-            this.comboBoxDrive.SelectedIndexChanged += new System.EventHandler(this.comboBoxDrive_SelectedIndexChanged);
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(31, 591);
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(7, 1588);
             this.progressBar1.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(748, 63);
+            this.progressBar1.Size = new System.Drawing.Size(1777, 35);
             this.progressBar1.TabIndex = 7;
             // 
             // comboBoxBackup
             // 
             this.comboBoxBackup.FormattingEnabled = true;
-            this.comboBoxBackup.Location = new System.Drawing.Point(238, 150);
+            this.comboBoxBackup.Location = new System.Drawing.Point(168, 115);
             this.comboBoxBackup.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.comboBoxBackup.Name = "comboBoxBackup";
             this.comboBoxBackup.Size = new System.Drawing.Size(339, 49);
@@ -130,7 +135,7 @@ namespace Encore
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(5, 52);
+            this.label1.Location = new System.Drawing.Point(7, 8);
             this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(116, 41);
@@ -140,7 +145,7 @@ namespace Encore
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 150);
+            this.label2.Location = new System.Drawing.Point(7, 115);
             this.label2.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(176, 41);
@@ -150,7 +155,7 @@ namespace Encore
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(867, 52);
+            this.label3.Location = new System.Drawing.Point(7, 0);
             this.label3.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(306, 41);
@@ -160,7 +165,7 @@ namespace Encore
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(867, 656);
+            this.label4.Location = new System.Drawing.Point(7, 765);
             this.label4.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(267, 41);
@@ -170,38 +175,129 @@ namespace Encore
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(31, 530);
+            this.label5.Location = new System.Drawing.Point(7, 1530);
             this.label5.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(140, 41);
+            this.label5.Size = new System.Drawing.Size(133, 41);
             this.label5.TabIndex = 13;
-            this.label5.Text = "Progress:";
+            this.label5.Text = "Progress";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 600F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(5, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(2397, 1637);
+            this.tableLayoutPanel1.TabIndex = 14;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.progressBar1, 0, 5);
+            this.tableLayoutPanel3.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel3.Controls.Add(this.dataGridView1, 0, 3);
+            this.tableLayoutPanel3.Controls.Add(this.dataGridView2, 0, 1);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(603, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 6;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(1791, 1631);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.AutoSize = true;
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(594, 1631);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel2.Controls.Add(this.buttonCompare);
+            this.panel2.Controls.Add(this.buttonBackup);
+            this.panel2.Location = new System.Drawing.Point(43, 471);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(507, 279);
+            this.panel2.TabIndex = 15;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.Controls.Add(this.comboBoxBackup);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBoxDrive);
+            this.panel1.Location = new System.Drawing.Point(43, 84);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(507, 238);
+            this.panel1.TabIndex = 15;
+            // 
+            // comboBoxDrive
+            // 
+            this.comboBoxDrive.FormattingEnabled = true;
+            this.comboBoxDrive.Location = new System.Drawing.Point(168, 0);
+            this.comboBoxDrive.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
+            this.comboBoxDrive.Name = "comboBoxDrive";
+            this.comboBoxDrive.Size = new System.Drawing.Size(339, 49);
+            this.comboBoxDrive.TabIndex = 6;
+            this.comboBoxDrive.SelectedIndexChanged += new System.EventHandler(this.comboBoxDrive_SelectedIndexChanged);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(2404, 1651);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.dataGridView2);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBoxBackup);
-            this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.comboBoxDrive);
-            this.Controls.Add(this.buttonBackup);
-            this.Controls.Add(this.buttonCompare);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.DoubleBuffered = true;
             this.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.Name = "FormMain";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -210,7 +306,6 @@ namespace Encore
         private System.Windows.Forms.Button buttonCompare;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button buttonBackup;
-        private System.Windows.Forms.ComboBox comboBoxDrive;
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.ComboBox comboBoxBackup;
@@ -219,6 +314,12 @@ namespace Encore
         private Label label3;
         private Label label4;
         private Label label5;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel3;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Panel panel1;
+        private ComboBox comboBoxDrive;
+        private Panel panel2;
     }
 }
 
